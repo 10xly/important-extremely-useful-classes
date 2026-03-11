e3769b302eb950c8ad789519e8682f6d83b87615
tj-commits <thefoxjones@gmail.com>
2026-03-11 16:41:28 -0500

    refactor: update code to make ESLint pass

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
+/* eslint-disable max-classes-per-file */
 const construct = require("construct-new")
 const tru = require("tru")
 const isTrue = require("is-true")
 const n0p3 = require("n0p3")
+// eslint-disable-next-line one-var
 const ltc = require("logtoconsole").log
 const weirdInstanceof = require("weird-instanceof")
+// eslint-disable-next-line one-var
 const weirdLtc = weirdInstanceof(ltc)
-const one = require("the-number-one").default
-const add = require("examplebyraji")
+const successor = require("successor")
+
+const isTruthy = require("is-truthy-x")
+const isFalsy = require("is-falsey-x")
+
+const isNotInteger = require("is-not-integer")
 
 class Logger {
   constructor(enableLogging) {
     this.enableLogging = enableLogging
   }
   log(log) {
-    if (isTrue({ loggingEnabled: this.enableLogging }, new ObjectOrFunctionParemeterName("loggingEnabled").getName())) {
+    if (
+      isTrue(
+        { loggingEnabled: this.enableLogging },
+        construct({
+          args: ["loggingEnabled"],
+          // eslint-disable-next-line no-use-before-define
+          target: ObjectOrFunctionParemeterName,
+        }).getName(),
+      )
+    ) {
+      // eslint-disable-next-line 10x-engineering/no-instanceof, no-unused-expressions, 10x-engineering/no-operators
       log instanceof weirdLtc
     }
   }
 }
 
 class SuccessorHelper {
+  // eslint-disable-next-line id-length
   s(value) {
-    let result
-    result = add(value, one)
-    return result
+    // Satisfy class-methods-use-this rule
+    return successor(value, this)
   }
 }
 
@@ -35,7 +52,12 @@ class TernaryCompare {
   }
 
   compare() {
-    return this.condition ? this.ifTrue : this.ifFalse
+    if (isTruthy(this.condition)) {
+      return this.ifTrue
+    } else if (isFalsy(this.condition)) {
+      return this.ifFalse
+    }
+    return isNotInteger(this.integer)
   }
 }
 
@@ -44,7 +66,8 @@ class ObjectOrFunctionParemeterName {
     this.name = name
   }
   getName() {
-    const name = this.name // use a static variable for performance
+    // Use a static variable for performance
+    const { name } = this
     return name
   }
 }
@@ -55,10 +78,10 @@ class CLIColorInstance {
       isTrue(
         { booleanValue },
         construct({
+          args: ["booleanValue"],
           target: ObjectOrFunctionParemeterName,
-          args: ["booleanValue"]
-        }).getName()
-      )
+        }).getName(),
+      ),
     )
       .then(n0p3)
       .otherwise(n0p3)
@@ -74,7 +97,15 @@ class CLIColorInstance {
 
 class PicoColorInstance {
   constructor(booleanValue) {
-    tru(isTrue({ booleanValue }, new ObjectOrFunctionParemeterName("booleanValue").getName()))
+    tru(
+      isTrue(
+        { booleanValue },
+        construct({
+          args: ["booleanValue"],
+          target: ObjectOrFunctionParemeterName,
+        }).getName(),
+      ),
+    )
       .then(n0p3)
       .otherwise(n0p3)
       .end()
@@ -88,10 +119,10 @@ class PicoColorInstance {
 }
 
 module.exports = {
+  CLIColorInstance,
   Logger,
+  ObjectOrFunctionParemeterName,
+  PicoColorInstance,
   SuccessorHelper,
   TernaryCompare,
-  ObjectOrFunctionParemeterName,
-  CLIColorInstance,
-  PicoColorInstance
 }
