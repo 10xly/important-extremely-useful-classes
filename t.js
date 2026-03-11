4a5ca60b1c9cde5e653ba4bdb81b23bad10c069c
tj-commits <thefoxjones@gmail.com>
2026-03-11 16:29:03 -0500

    chore: delete t.js

--- t.js
+++
@@ -1,7 +0,0 @@
-var {NilGuardedExecutor} = require('./index')
-
-const guarded = new NilGuardedExecutor(console.log, "Hello", "World")
-guarded.execute()() // Logs "Hello World"
-
-const guardedFail = new NilGuardedExecutor(console.log, "Hello", null)
-guardedFail.execute()() // Does nothing
